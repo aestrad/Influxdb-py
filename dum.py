27beec1072b5459745b147f52a12fff4c9de474a
Abner <abestrad@outlook.com>
2020-06-29 16:46:31 +0200

    host comments

--- dum.py
+++ dum.py
@@ -2,7 +2,7 @@ from influxdb import InfluxDBClient
 from requests.exceptions import ConnectionError
 
 if __name__ == "__main__":
-    # Connect to Influxdb
+    # Connect to Influxdb    Host = service name
     try:
         client = InfluxDBClient('influx-py_influxdb_1', 8086, 'root', 'root', 'example')
         print(client.ping())
