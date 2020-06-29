13549554b9a5841ab6af26044bd4362f6082bc06
abner <aec@safeonline.dk>
2020-06-29 16:10:17 +0200

    basic example

---
+++ dum.py
@@ -0,0 +1,11 @@
+from influxdb import InfluxDBClient
+from requests.exceptions import ConnectionError
+
+if __name__ == "__main__":
+    # Connect to Influxdb
+    try:
+        client = InfluxDBClient('influx-py_influxdb_1', 8086, 'root', 'root', 'example')
+        print(client.ping())
+        print("Connected")
+    except ConnectionError as e:
+        print("No Database Connection")
